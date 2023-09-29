841dfcb5e2fc826109f751ebca20571a8f9be39c
GlamourPundaren <borg.martin@outlook.com>
2023-09-29 10:27:52 +0200

    ändrat lite

---
+++ Listor/js/main.js
@@ -0,0 +1,12 @@
+window.onload = () => {};
+
+function handleClick() {}
+
+const numbers = [1, 1, 2, 3, 5, 8, 13];
+console.log("Listan är:", numbers.length, "lång");
+
+console.log("Värdet på position5:", numbers[5]);
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
