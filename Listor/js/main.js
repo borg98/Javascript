5a410fd91292466d1ed1ed6cd60a6906ba40c3a5
GlamourPundaren <borg.martin@outlook.com>
2023-09-29 10:35:39 +0200

    123

--- Listor/js/main.js
+++ Listor/js/main.js
@@ -2,11 +2,27 @@ window.onload = () => {};
 
 function handleClick() {}
 
+function printList() {
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
+}
+
 const numbers = [1, 1, 2, 3, 5, 8, 13];
 console.log("Listan är:", numbers.length, "lång");
 
 console.log("Värdet på position5:", numbers[5]);
 
-for (let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
-}
+printList();
+
+numbers.push(21);
+
+printList();
+
+numbers.pop();
+
+printList();
+
+numbers.splice(1, 1);
+
+printList();
